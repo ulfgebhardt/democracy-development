5ef748638341e76aeda165232d1f101a85d11ec8
ManAnRuck <github@manuelruck.de>
2020-06-24 10:03:04 +0200

    add democracy api-redirect
    Signed-off-by: ManAnRuck <github@manuelruck.de>

---
+++ democracy-app.de-api-redirect/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import request from "request";
+
+const app = express();
+
+app.all("*", (req, res) => {
+  console.log("R");
+  // const url = "http://localhost:8000/#/";
+  const url = process.env.REDIRECT_URL!;
+  return req
+    .pipe(
+      request(url, { qs: req.query }).on("error", function (err) {
+        console.info(err);
+        return res.sendStatus(400);
+      })
+    )
+    .pipe(res);
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
